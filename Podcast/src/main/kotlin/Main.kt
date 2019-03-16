75dff0214e2f7e51f9c398962381690f57f63dd8
Richie Zhang <richiezhang256@gmail.com>
2019-03-15 21:23:07 -0700

    Ask for the download location.

--- Podcast/src/main/kotlin/Main.kt
+++ Podcast/src/main/kotlin/Main.kt
@@ -1,12 +1,20 @@
 fun main(args: Array<String>) {
 
     var url: String
+    var outputDir : String
 
+    // Ask for the RSS URL
     do {
         print("Podcast RSS: ")
         url = readLine()!!
     } while (url == "")
 
+    // Ask for the download location
+    print("Download to (download):")
+    outputDir = readLine()!!
+    if (outputDir == "") {
+        outputDir = "download"
+    }
 
-    println("TODO: Download podcasts from $url")
+    println("TODO: Download podcasts from $url to $outputDir")
 }
