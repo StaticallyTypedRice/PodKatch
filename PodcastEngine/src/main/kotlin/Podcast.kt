20e971dde967bdf8a312a7f7a4c3da5de48a33ca
Richie Zhang <richiezhang256@gmail.com>
2019-03-15 22:00:48 -0700

    Create the PodcastEpisode class.

--- PodcastEngine/src/main/kotlin/Podcast.kt
+++ PodcastEngine/src/main/kotlin/Podcast.kt
@@ -1,4 +1,5 @@
 import java.net.URL
+import java.util.Date
 
 package podcastengine
 
@@ -9,4 +10,17 @@ class Podcast {
     var description: String?    // The podcast description
     var copyright: String?      // The copyright notice
     var image: URL?             // The cover image
+}
+
+class PodcastEpisode {
+    var podcast: Podcast        // The podcast where the episode originates
+
+    var guid: String?           // The episode GUID
+    var description: String?    // The episode description
+    var pubDate: Date?          // The epidode publication date
+    var duration: String?       // The epidode duration (TODO: Use a dedicated data type)
+
+    var file: URL               // The episode file URL
+    var fileLength:Long?        // The episode file length
+    var fileType:String?        // The episode file type
 }
