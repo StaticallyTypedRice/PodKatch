3b25a41962495fd486018a51f7c7f189d615e927
Richie Zhang <richiezhang256@gmail.com>
2019-03-17 13:46:33 -0700

    Update podcast classes to use constructors for required variables and set defaults for optional values.

--- PodcastEngine/src/main/kotlin/Podcast.kt
+++ PodcastEngine/src/main/kotlin/Podcast.kt
@@ -3,16 +3,14 @@ package podcastengine.podcast
 import java.net.URL
 import java.util.Date
 
-class Podcast {
-    var title: String           // The podcast title
-    var rss: URL                // The podcast RSS URL
-    var link: URL?              // The link to the podcast page
-    var description: String?    // The podcast description
-    var copyright: String?      // The copyright notice
-    var image: URL?             // The cover image
+class Podcast(title: String, rss: URL) {
+    var link: URL? = null               // The link to the podcast page
+    var description: String? = null     // The podcast description
+    var copyright: String? = null       // The copyright notice
+    var image: URL? = null              // The cover image
 
     // All podcast episodes
-    var episodes: Array<Episode>
+    var episodes: Array<Episode> = arrayOf<Episode>()
 
     fun createFromRSS(file: String) {
         // Fill in the above data from an RSS file
@@ -20,15 +18,14 @@ class Podcast {
     }
 }
 
-class Episode {
-    var guid: String?           // The episode GUID
-    var description: String?    // The episode description
-    var pubDate: Date?          // The episode publication date
-    var duration: String?       // The episode duration (TODO: Use a dedicated data type)
+class Episode(file: URL) {
+    var guid: String? = null        // The episode GUID
+    var description: String? = null // The episode description
+    var pubDate: Date? = null       // The episode publication date
+    var duration: String? = null    // The episode duration (TODO: Use a dedicated data type)
 
-    var file: URL               // The episode file URL
-    var fileLength:Long?        // The episode file length
-    var fileType:String?        // The episode file type
+    var fileLength:Long? = null     // The episode file length
+    var fileType:String? = null     // The episode file type
 
     fun download(outputDir: String) {
         // Download the episode
