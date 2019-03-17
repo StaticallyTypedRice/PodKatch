867db8044be7197c8a3a0ce9f651c73f8012f7a2
Richie Zhang <richiezhang256@gmail.com>
2019-03-17 15:15:11 -0700

    Populate podcast metadata from RSS.
    TODO: Populate episode metadata.

--- PodcastEngine/src/main/kotlin/Podcast.kt
+++ PodcastEngine/src/main/kotlin/Podcast.kt
@@ -2,22 +2,26 @@ package podcastengine.podcast
 
 import java.net.URL
 import java.util.Date
-import org.w3c.dom.Document
 
-
-class Podcast(title: String, rss: Document) {
+class Podcast {
+    var title: String? = null           // The podcast title
     var link: URL? = null               // The link to the podcast page
     var description: String? = null     // The podcast description
-    var copyright: String? = null       // The copyright notice
     var image: URL? = null              // The cover image
+    var author: String? = null          // The podcast author
+    var category: String? = null        // The podcast category
+
+    // Whether the podcast is set as explicit for iTunes
+    // Set by the author, not iTunes
+    // Doesn't prove that the podcast isn't explicit if false or null
+    /// Not guaranteed to be an accurate indicator, not sure if moderated by iTunes (probably not)
+    var itunesExplicit: Boolean? = null
+
+    var language: String? = null        // The podcast language
+    var copyright: String? = null       // The copyright notice
 
     // All podcast episodes
-    var episodes: Array<Episode> = arrayOf<Episode>()
-
-    fun createFromRSS() {
-        // Fill in the above data from an RSS file
-        //TODO
-    }
+    var episodes: Array<Episode> = arrayOf()
 }
 
 class Episode(file: URL) {
@@ -25,6 +29,14 @@ class Episode(file: URL) {
     var description: String? = null // The episode description
     var pubDate: Date? = null       // The episode publication date
     var duration: String? = null    // The episode duration (TODO: Use a dedicated data type)
+
+    var episodeType: String? = null // The episode type (based on iTunes' episode types)
+
+    // Whether the podcast is set as explicit for iTunes
+    // Set by the author, not iTunes
+    // Doesn't prove that the podcast isn't explicit if false or null
+    // Not guaranteed to be an accurate indicator, not sure if moderated by iTunes (probably not)
+    var itunesExplicit: Boolean? = null
 
     var fileLength:Long? = null     // The episode file length
     var fileType:String? = null     // The episode file type
--- PodcastEngine/src/main/kotlin/RSS.kt
+++ PodcastEngine/src/main/kotlin/RSS.kt
@@ -4,6 +4,9 @@ import java.net.URL
 import java.io.File
 import javax.xml.parsers.DocumentBuilderFactory
 import org.w3c.dom.Document
+import org.w3c.dom.Element
+import org.w3c.dom.NodeList
+import podcastengine.podcast.*
 
 fun getRemoteRSS(url: URL) {
     //TODO
@@ -18,3 +21,34 @@ fun getLocalRSS(path: String): Document {
     return RSSDocument
 }
 
+fun createPodcastFromRSS(RSS: Document): Podcast {
+    // Create a Podcast object from an RSS document
+
+    // Parse the podcast metadata
+    val channel: Element = RSS.getElementsByTagName("rss").item(0) as Element
+
+    val podcast = Podcast()
+
+    podcast.title = channel.getElementsByTagName("title").item(0).textContent
+    podcast.link = URL(channel.getElementsByTagName("link").item(0).textContent)
+    podcast.description = channel.getElementsByTagName("description").item(0).textContent
+    podcast.image = URL(channel.getElementsByTagName("itunes:image").item(0).attributes.getNamedItem("href").textContent)
+    podcast.author = channel.getElementsByTagName("itunes:author").item(0).textContent
+    podcast.category = channel.getElementsByTagName("itunes:category").item(0).attributes.getNamedItem("text").textContent
+
+    when(channel.getElementsByTagName("itunes:author").item(0).textContent) {
+        "yes" -> podcast.itunesExplicit = true
+        "no" -> podcast.itunesExplicit = false
+    }
+
+    podcast.language = channel.getElementsByTagName("language").item(0).textContent
+    podcast.copyright = channel.getElementsByTagName("copyright").item(0).textContent
+
+    // Parse the podcast episodes
+    val episodes: NodeList = RSS.getElementsByTagName("item")
+    for (i in 0 until episodes.length) {
+         continue //TODO
+    }
+
+    return podcast
+}
