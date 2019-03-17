36e03b0637cd20f2d953e3ebb1066cb426ec40c0
Richie Zhang <richiezhang256@gmail.com>
2019-03-17 12:13:25 -0700

    Ask if the RSS file is remote or local in the CLI.

--- CLI/src/main/kotlin/Main.kt
+++ CLI/src/main/kotlin/Main.kt
@@ -1,15 +1,40 @@
 package podkatch.cli
 
+import java.lang.NumberFormatException
+
 fun main(args: Array<String>) {
 
-    var url: String
-    var outputDir : String
+    var location: String
+    var outputDir: String
+    var remoteRSS: Boolean
+    var remoteRSSInput: String?
 
-    // Ask for the RSS URL
+    // Ask if the RSS file is remote or local
+    do {
+        println("Is the RSS file remote or local?\n1: Remote (default)    2: Local\n")
+        remoteRSSInput = readLine()
+
+        if ((remoteRSSInput == "1") || (remoteRSSInput == "")) {
+            remoteRSS = true
+        } else if (remoteRSSInput == "2") {
+            remoteRSS = false
+        } else {
+            println()
+            println("Invalid input. Please enter '1', '2', or a blank line.")
+        }
+
+        // If the input is a blank line, there is no need to print another one.
+        if (remoteRSSInput != "") {
+            println()
+        }
+
+    } while ((remoteRSSInput != "") && (remoteRSSInput != "1") && (remoteRSSInput != "2"))
+
+    // Ask for the RSS location
     do {
         print("Podcast RSS: ")
-        url = readLine()!!
-    } while (url == "")
+        location = readLine()!!
+    } while (location == "")
 
     // Ask for the download location
     print("Download to (download): ")
@@ -18,5 +43,5 @@ fun main(args: Array<String>) {
         outputDir = "download"
     }
 
-    println("TODO: Download podcasts from $url to $outputDir")
+    println("TODO: Download podcasts from $location to $outputDir")
 }
