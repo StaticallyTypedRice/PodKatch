acadf493e45c0c9e66ded6e7e4e7ffb20c52af7b
Richie Zhang <richiezhang256@gmail.com>
2019-03-17 12:58:09 -0700

    Create the function for retrieving a local RSS file.

--- PodcastEngine/src/main/kotlin/RSS.kt
+++ PodcastEngine/src/main/kotlin/RSS.kt
@@ -1,12 +1,19 @@
 package podcastengine.rss
 
 import java.net.URL
+import java.io.File
+import javax.xml.parsers.DocumentBuilderFactory
 
 fun getRemoteRSS(url: URL) {
     //TODO
 }
 
 fun getLocalRSS(path: String) {
-    //TODO
+    val RSSFile: InputStream = File(path)
+    val RSSDocument: Document = DocumentBuilderFactory.newInstance().newDocumentBuilder().parse(RSSFile)
+
+    RSSDocument.documentElement.normalize()
+
+    return RSSDocument
 }
 
