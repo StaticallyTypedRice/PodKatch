6e37977bf37ad4cc5d25da4ddef5c641f3d1b056
Richie Zhang <richiezhang256@gmail.com>
2019-03-29 09:31:54 -0700

    Update a comment.

--- PodcastEngine/src/main/kotlin/Podcast.kt
+++ PodcastEngine/src/main/kotlin/Podcast.kt
@@ -36,9 +36,9 @@ class Episode(_title: String, _file: URL) {
     var duration: Duration? = null  // The episode duration
     var episodeType: String? = null // The episode type (based on iTunes' episode types)
 
-    // Whether the podcast is set as explicit for iTunes
+    // Whether the episode is set as explicit for iTunes
     // Set by the author, not iTunes
-    // Doesn't prove that the podcast isn't explicit if false or null
+    // Doesn't prove that the episode isn't explicit if false or null
     // Not guaranteed to be an accurate indicator, not sure if moderated by iTunes (probably not)
     var itunesExplicit: Boolean? = null
 
