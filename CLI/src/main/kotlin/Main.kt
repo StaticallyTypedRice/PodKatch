5242269ea9abf1a43203bca45e08ad2063e58443
Richie Zhang <richiezhang256@gmail.com>
2019-05-21 20:28:10 -0700

    Refactor names to a more consistent capitalization scheme.

--- CLI/src/main/kotlin/Main.kt
+++ CLI/src/main/kotlin/Main.kt
@@ -22,30 +22,30 @@ fun main(args: Array<String>) {
 
     var location: String
     var outputDir: String
-    var remoteRSS: Boolean = false
-    var remoteRSSInput: String?
+    var remoteRss: Boolean = false
+    var remoteRssInput: String?
 
     // Ask if the RSS file is remote or local
     do {
         println("Is the RSS file remote or local?")
         println("1: Remote (default)    2: Local")
-        remoteRSSInput = readLine()
+        remoteRssInput = readLine()
 
-        if ((remoteRSSInput == "1") || (remoteRSSInput == "")) {
-            remoteRSS = true
-        } else if (remoteRSSInput == "2") {
-            remoteRSS = false
+        if ((remoteRssInput == "1") || (remoteRssInput == "")) {
+            remoteRss = true
+        } else if (remoteRssInput == "2") {
+            remoteRss = false
         } else {
             println()
             println("Invalid input. Please enter '1', '2', or a blank line.")
         }
 
         // If the input is a blank line, there is no need to print another one.
-        if (remoteRSSInput != "") {
+        if (remoteRssInput != "") {
             println()
         }
 
-    } while ((remoteRSSInput != "") && (remoteRSSInput != "1") && (remoteRSSInput != "2"))
+    } while ((remoteRssInput != "") && (remoteRssInput != "1") && (remoteRssInput != "2"))
 
     // Ask for the RSS location
     do {
@@ -60,11 +60,11 @@ fun main(args: Array<String>) {
         outputDir = "download"
     }
 
-    if (remoteRSS) {
-        val request: Request = getRemoteRSS(URL(location))
+    if (remoteRss) {
+        val request: Request = getRemoteRss(URL(location))
         val (rssRequest, rssResponse, rssRresult) = request.response()
         val (rssBytes, rssError) = rssRresult
-        val rssFile: Document = parseRSS(rssBytes)
+        val rssFile: Document = parseRss(rssBytes)
 
         download(rssFile, outputDir)
 
@@ -73,7 +73,7 @@ fun main(args: Array<String>) {
 
         do {
             try {
-                val rssFile = parseRSS(getLocalRSS(location))
+                val rssFile = parseRss(getLocalRss(location))
                 fileValid = true
                 download(rssFile, outputDir)
             } catch (e: FileNotFoundException) {
@@ -94,7 +94,7 @@ fun main(args: Array<String>) {
 }
 
 fun download(RSS: Document, downloadTo: String) {
-    val podcast = createPodcastFromRSS(RSS)
+    val podcast = createPodcastFromRss(RSS)
 
     for (episode in podcast.episodes) {
         episode.download(downloadTo)
