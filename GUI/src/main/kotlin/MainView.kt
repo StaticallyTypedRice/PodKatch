7cf9cffb0167cff3eebe9e538e135fdd6cab3bdb
Richie Zhang <richiezhang256@gmail.com>
2019-05-23 19:18:42 -0700

    Create a GUI structure with placeholders.

--- GUI/src/main/kotlin/MainView.kt
+++ GUI/src/main/kotlin/MainView.kt
@@ -3,7 +3,18 @@ package kodkatch.gui.views
 import tornadofx.*
 
 class MainView : View("PodKatch") {
-    override val root = borderpane {
+    override val root = hbox {
+        val sidebar = vbox {
+            val addPodcastBtn = button("Add podcast")
+            val updateBtn = button("Update podcasts")
+            label("The sidebar.")
+        }
+        val window = vbox {
+            label("Everything else will go here.")
+            val playerControls = hbox {
+                label("Play/pause controls")
+            }
+        }
 
     }
 }
