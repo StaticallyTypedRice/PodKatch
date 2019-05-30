4a49a26d510e00378f7ffbbc5396ffc8162ef1f3
richie <richiezhang256@gmail.com>
2019-05-29 18:32:57 -0700

    Add light theme styles for the sidebar.

--- GUI/src/main/kotlin/views/MainView.kt
+++ GUI/src/main/kotlin/views/MainView.kt
@@ -3,26 +3,33 @@ package kodkatch.gui.views
 import tornadofx.*
 
 class MainView : View("PodKatch Podcast Client") {
-    override val root = hbox {
+    override val root = borderpane {
         style {
             importStylesheet("/css/third-party/goliath/Goliath-Base.css")
             importStylesheet("/css/third-party/goliath/Goliath-Light.css")
             importStylesheet("/css/PodKatch-Main.css")
             importStylesheet("/css/PodKatch-Light.css")
         }
-        val sidebar = vbox {
-            val addPodcastBtn = button("Add podcast")
-            val updateBtn = button("Update podcasts")
-            separator()
-            label("Subscribed podcasts")
-            separator()
-            label("Everything else in the sidebar.")
+        val sidebar = left{
+            vbox {
+                addClass("sidebar")
 
+                val addPodcastBtn = button("Add podcast")
+                val updateBtn = button("Update podcasts")
+                separator()
+                label("Subscribed podcasts")
+                separator()
+                label("Everything else in the sidebar.")
+            }
         }
-        val window = vbox {
-            label("Everything else will go here.")
-            val playerControls = hbox {
-                label("Play/pause controls")
+        val window = right {
+            vbox {
+                addClass("window")
+
+                label("Everything else will go here.")
+                val playerControls = hbox {
+                    label("Play/pause controls")
+                }
             }
         }
 
