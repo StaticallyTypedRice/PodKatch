518678826517eb7e73827bb91038fd89fc6b8bf7
Richie Zhang <richiezhang256@gmail.com>
2019-05-30 20:58:37 -0700

    Add the player controls to a borderplane bottom element.

--- GUI/src/main/kotlin/views/Window.kt
+++ GUI/src/main/kotlin/views/Window.kt
@@ -3,6 +3,13 @@ package podkatch.gui.views
 import tornadofx.*
 
 class Window : View() {
+    override val root = borderpane {
+        top(WindowMain::class)
+        bottom(PlayerControls::class)
+    }
+}
+
+class WindowMain : View() {
     override val root = vbox {
         addClass("window")
 
