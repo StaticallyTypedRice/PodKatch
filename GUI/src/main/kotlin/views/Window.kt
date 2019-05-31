f7b66b5456a405206dec92880cba990713ab6db6
Richie Zhang <richiezhang256@gmail.com>
2019-05-30 21:01:39 -0700

    Merge views WindowMain with Window.

--- GUI/src/main/kotlin/views/Window.kt
+++ GUI/src/main/kotlin/views/Window.kt
@@ -4,16 +4,14 @@ import tornadofx.*
 
 class Window : View() {
     override val root = borderpane {
-        top(WindowMain::class)
+        top {
+            vbox {
+                addClass("window")
+
+                label("Everything else will go here.")
+                button("Placeholder button")
+            }
+        }
         bottom(PlayerControls::class)
-    }
-}
-
-class WindowMain : View() {
-    override val root = vbox {
-        addClass("window")
-
-        label("Everything else will go here.")
-        button("Placeholder button")
     }
 }
