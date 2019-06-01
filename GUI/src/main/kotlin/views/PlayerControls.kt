a2e136ec1c3c8fd1d83bef6c2ceac6362bcabbd9
Richie Zhang <richiezhang256@gmail.com>
2019-05-31 17:15:17 -0700

    Add playback buttons.

--- GUI/src/main/kotlin/views/PlayerControls.kt
+++ GUI/src/main/kotlin/views/PlayerControls.kt
@@ -1,10 +1,32 @@
 package podkatch.gui.views
 
 import tornadofx.*
+import org.kordamp.ikonli.javafx.FontIcon
+import tornadofx.WizardStyles.Companion.graphic
 
 class PlayerControls : View() {
     override val root = hbox {
         addClass("player")
-        label("Play/pause controls")
+        val backwardBtn = button {
+            graphic = FontIcon().apply {
+                iconLiteral="ti-control-backward"
+                iconSize=25
+            }
+
+        }
+        val playBtn = button {
+            graphic = FontIcon().apply {
+                iconLiteral="ti-control-play"
+                iconSize=25
+            }
+
+        }
+        val forwardBtn = button {
+            graphic = FontIcon().apply {
+                iconLiteral="ti-control-forward"
+                iconSize=25
+            }
+
+        }
     }
 }
