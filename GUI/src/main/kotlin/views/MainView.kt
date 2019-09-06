99766ddba7fffb0a92394eb39664e46e6fa2d036
Richie Zhang <richiezhang256@gmail.com>
2019-09-06 15:08:53 -0700

    Set the stage icon.

--- GUI/src/main/kotlin/views/MainView.kt
+++ GUI/src/main/kotlin/views/MainView.kt
@@ -1,10 +1,12 @@
 package kodkatch.gui.views
 
+import javafx.scene.image.Image
 import podkatch.gui.views.*
 import tornadofx.*
 
 class MainView : View("PodKatch Podcast Client") {
     override val root = borderpane {
+        setStageIcon(Image("file:/img/icon.png"))
         style {
             importStylesheet("/css/third-party/goliath/Goliath-Base.css")
             importStylesheet("/css/third-party/goliath/Goliath-Light.css")
