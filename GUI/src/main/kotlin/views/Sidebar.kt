3375d7a2e9d488f02b33b78e5575178976b92fbd
richie <richiezhang256@gmail.com>
2019-09-06 21:29:15 -0700

    Create actions for the sidebar buttons.

--- GUI/src/main/kotlin/views/Sidebar.kt
+++ GUI/src/main/kotlin/views/Sidebar.kt
@@ -6,8 +6,16 @@ class Sidebar : View() {
     override val root = vbox {
         addClass("sidebar")
 
-        val addPodcastBtn = button("Add podcast")
-        val updateBtn = button("Update podcasts")
+        val addPodcastBtn = button("Add podcast") {
+            action {
+                println("TODO: Create a dialog box for adding podcasts.")
+            }
+        }
+        val updateBtn = button("Update podcasts") {
+            action {
+                println("TODO: Automatically update all subscribed RSS files and download new podcast episodes.")
+            }
+        }
         separator()
         label("Subscribed podcasts")
         separator()
