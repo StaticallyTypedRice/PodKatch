c07cc882d3ffef359443036989abe2355b5839fc
richie <richiezhang256@gmail.com>
2019-09-06 20:52:24 -0700

    Add the 'feature-gui-css' branch to the git scripts.

--- scripts/git-pull-all.cmd
+++ scripts/git-pull-all.cmd
@@ -9,5 +9,8 @@ git pull origin feature-cli
 git checkout feature-gui
 git pull origin feature-gui
 
+git checkout feature-gui-css
+git pull origin feature-gui-css
+
 git checkout feature-engine
 git pull origin feature-engine
