0913c5d519ce98281d072d1cac56f56577c678dc
Richie Zhang <richiezhang256@gmail.com>
2019-09-09 10:42:54 -0700

    Create UI elements in view AddPodcastDialog.

--- GUI/src/main/kotlin/views/dialogs/AddPodcastDialog.kt
+++ GUI/src/main/kotlin/views/dialogs/AddPodcastDialog.kt
@@ -4,6 +4,38 @@ import tornadofx.*
 
 class AddPodcastDialog : View("Add Podcast") {
     override val root = vbox {
-        label("Add Podcast")
+        setMaxSize(500.toDouble(), 250.toDouble())
+
+        label("Enter the url of a podcast RSS file:")
+        hbox {
+            val rssUrlField = textfield {
+                //minWidth(450.toDouble())
+            }
+            val rssUrlSubmit = button("OK") {
+                tooltip("Subscribe to the podcast at this URL")
+                action {
+                    println("RSS URL: ${rssUrlField.text}")
+                }
+            }
+        }
+
+        label("Or:")
+        hbox {
+            button("Browse or Search iTunes") {
+                tooltip("Subscribe to a podcast from iTunes")
+                action {
+
+                }
+            }
+
+            button("Import From File") {
+                tooltip("Import a local RSS file")
+                action {
+
+                }
+            }
+
+        }
     }
 }
+
