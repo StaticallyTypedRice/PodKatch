5512cbb4ed992859e225e3021b382f8ea39d985f
richie <richiezhang256@gmail.com>
2019-09-09 21:22:41 -0700

    Create functions for subscribing to podcasts.

---
+++ GUI/src/main/kotlin/Subscribe.kt
@@ -0,0 +1,16 @@
+package podkatch.gui.subscribe
+
+import java.io.File
+import java.net.URL
+
+/**
+ * Subscribe to a podcast given the RSS file.
+ *
+ * @param source The source of the podcast.
+ */
+fun subscribeFromRss(source: URL) {
+    println("TODO")
+}
+fun subscribeFromRss(source: File) {
+    println("TODO")
+}
--- GUI/src/main/kotlin/views/dialogs/AddPodcastDialog.kt
+++ GUI/src/main/kotlin/views/dialogs/AddPodcastDialog.kt
@@ -1,6 +1,9 @@
 package podkatch.gui.views.dialogs
 
+import podkatch.gui.subscribe.*
+
 import tornadofx.*
+import java.net.URL
 
 class AddPodcastDialog : View("Subscribe to a Podcast") {
     override val root = vbox(2) {
@@ -16,6 +19,10 @@ class AddPodcastDialog : View("Subscribe to a Podcast") {
                 tooltip("Subscribe to the podcast at this URL")
                 action {
                     println("RSS URL: ${rssUrlField.text}")
+
+                    if (rssUrlField.text.isNotEmpty()) {
+                        subscribeFromRss(URL(rssUrlField.text))
+                    }
                 }
             }
         }
@@ -36,8 +43,8 @@ class AddPodcastDialog : View("Subscribe to a Podcast") {
             val podcastPlatformSources = arrayOf("iTunes", "gPodder", "fyyd")
 
             podcastPlatformSources.forEach {
-                button("Browse or Search ${it}") {
-                    tooltip("Subscribe to a podcast from ${it}")
+                button("Browse or Search $it") {
+                    tooltip("Subscribe to a podcast from $it")
                     action {
 
                     }
