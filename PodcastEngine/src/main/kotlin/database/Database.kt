b8f69624bfe8f8a5b8cc39593a571fd866c92f93
Richie Zhang <richiezhang256@gmail.com>
2019-10-04 16:25:02 -0700

    Move database source files from modules GUI to PodcastEngine.

---
+++ PodcastEngine/src/main/kotlin/database/Database.kt
@@ -0,0 +1,8 @@
+package podcastengine.database
+
+import com.squareup.sqldelight.db.SqlDriver
+import com.squareup.sqldelight.sqlite.driver.JdbcSqliteDriver
+import com.squareup.sqldelight.sqlite.driver.JdbcSqliteDriver.Companion.IN_MEMORY
+
+val driver: SqlDriver = JdbcSqliteDriver(IN_MEMORY)
+
