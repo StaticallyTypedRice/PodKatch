8af0cd7ab8c47220ecf33e3300007650de7b6001
richie <richiezhang256@gmail.com>
2019-12-29 22:13:07 -0800

    Add the JetBrains Exposed library as a dependency.

--- PodcastEngine/src/main/kotlin/database/Database.kt
+++ PodcastEngine/src/main/kotlin/database/Database.kt
@@ -4,6 +4,9 @@ import com.squareup.sqldelight.db.SqlDriver
 import com.squareup.sqldelight.sqlite.driver.JdbcSqliteDriver
 import com.squareup.sqldelight.sqlite.driver.JdbcSqliteDriver.Companion.IN_MEMORY
 
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.transaction
+
 //import podcastengine.database.MainDatabase
 
 val driver: SqlDriver = JdbcSqliteDriver(IN_MEMORY)
@@ -11,3 +14,4 @@ val driver: SqlDriver = JdbcSqliteDriver(IN_MEMORY)
 //MainDatabase.Schema.create(driver)
 
 //val database = MainDatabase(driver)
+
