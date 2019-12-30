96c265d7ecb78644af82d4d96c172c195f932aeb
richie <richiezhang256@gmail.com>
2019-12-29 18:33:53 -0800

    Define a database.

--- PodcastEngine/src/main/kotlin/database/Database.kt
+++ PodcastEngine/src/main/kotlin/database/Database.kt
@@ -4,5 +4,10 @@ import com.squareup.sqldelight.db.SqlDriver
 import com.squareup.sqldelight.sqlite.driver.JdbcSqliteDriver
 import com.squareup.sqldelight.sqlite.driver.JdbcSqliteDriver.Companion.IN_MEMORY
 
+//import podcastengine.database.MainDatabase
+
 val driver: SqlDriver = JdbcSqliteDriver(IN_MEMORY)
 
+//MainDatabase.Schema.create(driver)
+
+//val database = MainDatabase(driver)
