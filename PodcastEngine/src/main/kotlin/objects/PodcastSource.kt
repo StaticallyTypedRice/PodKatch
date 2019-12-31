e8628b4cd88b89d3678ae48278d56de2420811a5
richie <richiezhang256@gmail.com>
2019-12-31 01:21:02 -0800

    Use an enum class for the podcast source type.

--- PodcastEngine/src/main/kotlin/objects/PodcastSource.kt
+++ PodcastEngine/src/main/kotlin/objects/PodcastSource.kt
@@ -3,17 +3,12 @@ package podcastengine.objects
 /**
  * An object for conveying the source of a podcast.
  *
- * @param _type A string containing the type of source.
+ * @param _type The type of source.
  *              This should be used to determine which object to create from the source variable (File, URL, etc).
- *              The strings are not case sensitive and will always be reduced to all lowercase.
- *
- *              Types (more will be added as support for them are implemented):
- *               - url
- *               - file
  *
  * @param _source A string containing the source
  */
-class PodcastSource(_type: String, _source: String) {
-    var type: String = _type.toLowerCase()
+class PodcastSource(_type: PodcastSourceType, _source: String) {
+    var type: PodcastSourceType = _type
     var source: String = _source
 }
